15e63de2e9f164dfe5e981415c790d1cfab5caa1
Nikita Isakov <isakov.developer@gmail.com>
2020-09-13 11:47:21 +0600

    Added reviews

---
+++ blocks/reviews.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @var object $reviews reviews data from JSON
+ */
+?>
+
+<div class="reviews">
+    <div class="reviews__content container">
+        <div class="row">
+            <div class="col-12">
+                <div class="reviews__title title">
+                    <?=$reviews->title?>
+                </div>
+                <div class="reviews__slider">
+                    <?foreach ($reviews->reviews as $review) {?>
+                        <div class="reviews__item">
+                            <img src="<?=$review?>" alt="" class="reviews__scan">
+                        </div>
+                    <?}?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(window).on('load', function () {
+        $('.reviews__slider').flickity({
+            cellAlign: 'left',
+            pageDots: false,
+        });
+    });
+</script>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $consultation = json_decode(file_get_contents('data/consultation.json'));
 $products = json_decode(file_get_contents('data/products.json'));
 $support = json_decode(file_get_contents('data/support.json'));
 $trust = json_decode(file_get_contents('data/trust.json'));
+$reviews = json_decode(file_get_contents('data/reviews.json'));
 /* data */
 
 /* page blocks */
@@ -15,5 +16,6 @@ include 'blocks/consultation.php';
 include 'blocks/products.php';
 include 'blocks/support.php';
 include 'blocks/trust.php';
+include 'blocks/reviews.php';
 include 'blocks/footer.php';
 /* page blocks */
